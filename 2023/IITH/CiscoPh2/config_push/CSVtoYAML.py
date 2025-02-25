43e0ccb7f1eb21685be10b4d32d726d51153018c
KarnChoudhary <KarnChoudhary@proton.me>
2025-02-25 14:50:47 +0530

    update

---
+++ 2023/IITH/CiscoPh2/config_push/CSVtoYAML.py
@@ -0,0 +1,39 @@
+import csv
+import yaml
+from datetime import datetime
+
+# Path to the CSV file
+csv_file = "hosts.csv"
+
+# Reading data from CSV file
+with open(csv_file, mode='r') as file:
+    csv_reader = csv.DictReader(file)
+    yaml_data = {}
+
+    # Converting CSV data to YAML format
+    for row in csv_reader:
+        host_data = {
+            'hostname': row['Hostname'],
+            'username': row['username'],
+            'password': row['password'],
+            'groups': [group.strip() for group in row['Groups'].split(',')]
+        }
+        yaml_data[row['Host']] = {
+            'hostname': host_data['hostname'],
+            'username': host_data['username'],
+            'password': host_data['password'],
+            'groups': host_data['groups']
+        }
+
+# Converting to YAML format
+yaml_output = yaml.dump(yaml_data, default_flow_style=False)
+
+# Generating timestamp
+timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+
+# Writing YAML output to a text file with timestamp in the file name
+output_file = f"hosts_yaml_output_{timestamp}.yaml"
+with open(output_file, 'w') as file:
+    file.write(yaml_output)
+
+print(f"YAML output saved to {output_file}")
